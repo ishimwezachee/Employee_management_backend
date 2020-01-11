27b5afeb9b11429dbb93d873c6f2f2883bd34b98
zachee <zacheeishimwe@gmail.com>
2020-01-11 16:50:58 +0200

    manager can edit the employee

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -15,6 +15,34 @@ let employee = new Employee({
   res.status(201).json(employee)
 }
 
+//=========================================== update employee
+exports.modify_employee = async (req,res,next)=>{
+    let body = {}
+    if(req.body.name){
+      body.name = req.body.name
+    }
+    if(req.body.nationalId){
+      body.nationalId = req.body.nationalId
+    }
+    if(req.body.phone){
+      body.phone= req.body.phone
+    }
+    if(req.body.email){
+      body.email = req.body.email
+    }
+    if(req.body.birth_date){
+      body.birth_date = req.body.birth_date
+    }
+    if(req.body.status){
+        body.status = req.body.status
+      }
+    if(req.body.position){
+        body.position = req.body.position
+    }
+   const _id = req.params.employeeId;
+   const employee = await Employee.findOneAndUpdate({ _id }, body);
+   res.status(200).json(employee)
+  }
 //=========================================== delete employee
 exports.delete_employee = async (req,res,next)=>{
     const _id = req.params.employeeId;
--- src/routes/employee.js
+++ src/routes/employee.js
@@ -4,4 +4,5 @@ const EmployeeController = require('../controllers/employee');
 
 router.post("/employees",EmployeeController.create_employee);
 router.delete("/employees/:employeeId",EmployeeController.delete_employee);
+router.patch("/employees/:employeeId",EmployeeController.modify_employee);
 module.exports = router
